b51979054aa2658a20ef3ce1d396e91ef39d1924
Bruno <bruno.ramos@ccc.ufcg.edu.br>
2023-10-09 17:09:49 -0300

    refactor: Lista de nomes de times no componente formulario vem agora via props

---
+++ organo/src/components/Colaborador/index.jsx
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = (props) => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/BrunoRRamos.png" alt="Bruno" />
+            </div>
+            <div className="rodape">
+                <h4>Bruno</h4>
+                <h5>BackEnd</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Colaborador;
